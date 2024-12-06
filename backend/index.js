2bf0beb4149a0c9f866daac54bfc7cd4e32d8573
BHAVESH GAVALI <bgavali@BHAVESHs-MacBook-Pro.local>
2024-12-06 02:10:11 -0500

    added basic routes / Vercel.json file

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
+const { TwitterApi } = require("twitter-api-v2");
+
 const dotenv = require("dotenv");
 
 dotenv.config();
+
+async function initializeTwitter() {
+  const client = new TwitterApi({
+    appKey: process.env.API_KEY,
+    appSecret: process.env.API_KEY_SECRET,
+  });
+
+  const link = await client.generateAuthLink(process.env.CALLBACK_URL);
+  console.log(link);
+}
+
+initializeTwitter();
 
 app.get("/", (req, res) => {
   res.send("HOME PAGE");
@@ -12,6 +26,6 @@ app.get("/authLink", async (req, res) => {
   res.send("authLink PAGE");
 });
 
-app.listen("3021", () => {
-  console.log("Server Listening on port 3021");
+app.listen("3022", () => {
+  console.log("Server Listening on port 3022");
 });
