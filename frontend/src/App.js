337c4580271602a4a208785e88d9eba8bf95858a
BHAVESH GAVALI <bgavali@BHAVESHs-MacBook-Pro.local>
2024-12-06 02:20:59 -0500

    fix: remove unnecessary import causing build issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  const authLink = `${process.env.REACT_APP_BACKEND_LINK}/authLink`;
+  console.log(authLink);
+  return (
+    <div className="App">
+      <a href={authLink}>Login</a>
+    </div>
+  );
 }
 
 export default App;
